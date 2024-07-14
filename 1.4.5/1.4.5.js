389a9049f54c3e21ada621165354f3c025febdf7
Xenia <andreeva19-91@mail.ru>
2024-07-14 23:56:38 +0300

    add task 1.4.5

---
+++ 1.4.5/1.4.5.js
@@ -0,0 +1,33 @@
+function optionalChaining (obj, chain) {
+    for (let i=0; i < chain.length; i++) {
+        for (let key in obj) {
+            if (chain[i] === key && chain.length === 1) {
+                return obj[key];
+            } else if (chain[i] === key) {
+                return optionalChaining(obj[key], chain.slice(i+1, chain.length));
+            }      
+        }
+        return undefined;
+    }
+}
+// Напишите функцию, которая принимает первым параметром объект, 
+// вторым - массив из цепочки свойств, по которому нужно пройти, чтобы получить значение.
+
+// Если какое-то из свойств не найдено - функция возвращает undefined.
+
+// Пример:
+
+const obj = {
+  a: {
+    b: {
+      c: {
+        d: 'Привет!'
+      }
+    }
+  }
+};
+console.log(optionalChaining(obj, ["a", "b", "c", "d"])); // Привет
+console.log(optionalChaining(obj, ["a", "b", "c", "d", "e"])); // undefined
+
+console.log(optionalChaining(obj, ["a", "c", "d"])); // undefined
+console.log(optionalChaining(obj, ["b", "d", "a"])); // undefined
